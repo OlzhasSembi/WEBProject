81e4996078269946bd3ecabb5e7b8f55ecfe4a7b
OlzhasSembi <64279048+OlzhasSembi@users.noreply.github.com>
2020-05-22 12:10:59 +0600

    Add files via upload

---
+++ app/Http/Middleware/checkIsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class checkIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+        if(!$user->isAdmin()){
+            return redirect()->route('home')->with('warning','You do not have access');
+        }
+        return $next($request);
+    }
+}
